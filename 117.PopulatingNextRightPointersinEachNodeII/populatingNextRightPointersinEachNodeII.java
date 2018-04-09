de191f0b9ff29a54a2c9aaaacb0eafdf4dc071a2
alleninwood <alleninwood@gmail.com>
2018-04-09 14:46:36 -0700

    14/19/18

---
+++ 117.PopulatingNextRightPointersinEachNodeII/populatingNextRightPointersinEachNodeII.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for binary tree with next pointer.
+ * public class TreeLinkNode {
+ *     int val;
+ *     TreeLinkNode left, right, next;
+ *     TreeLinkNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    //O(1) space complexity
+    public void connect(TreeLinkNode root) {
+        TreeLinkNode cur = root;
+        TreeLinkNode head = null;
+        TreeLinkNode pre = null;
+        
+        while (cur != null) {
+            while (cur != null) {
+                if (cur.left != null) {
+                    if (pre != null) {
+                        pre.next = cur.left;
+                    } else {
+                        head = cur.left;
+                    }
+                    pre = cur.left;
+                }
+                if (cur.right != null) {
+                    if (pre != null) {
+                        pre.next = cur.right;
+                    } else {
+                        head = cur.right;
+                    }
+                    pre = cur.right;
+                }
+                cur = cur.next;
+            }
+            cur = head;
+            head = null;
+            pre = null;
+        }
+    }
+}

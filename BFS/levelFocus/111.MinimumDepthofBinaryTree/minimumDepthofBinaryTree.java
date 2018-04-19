baf4bbabfe06f6c0d4bb227926f452459be22418
alleninwood <alleninwood@gmail.com>
2018-04-19 11:02:00 -0700

    19/04/18

---
+++ BFS/levelFocus/111.MinimumDepthofBinaryTree/minimumDepthofBinaryTree.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int minDepth = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            minDepth++;
+            while (size-- > 0) {
+                TreeNode cur = queue.poll();
+                if (cur.left == null && cur.right == null) {
+                    return minDepth;
+                }
+                if (cur.left != null) {
+                    queue.offer(cur.left);
+                }
+                if (cur.right != null) {
+                    queue.offer(cur.right);
+                }
+            }
+        }
+        return 0;
+    }
+}

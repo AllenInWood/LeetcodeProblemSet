d74d6c246a5ab6344652c8e9002ca8a201a40d27
alleninwood <alleninwood@gmail.com>
2018-06-30 23:17:33 -0700

    06/30/18

---
+++ Array/149.MaxPointsonaLine/maxPointsonaLine.java
@@ -0,0 +1,59 @@
+/**
+ * Definition for a point.
+ * class Point {
+ *     int x;
+ *     int y;
+ *     Point() { x = 0; y = 0; }
+ *     Point(int a, int b) { x = a; y = b; }
+ * }
+ */
+class Solution {
+    public int maxPoints(Point[] points) {
+        if (points == null || points.length == 0) {
+            return 0;
+        }
+        if (points.length <= 2) {
+            return points.length;
+        }
+        Map<Integer, Map<Integer, Integer>> map = new HashMap<>();
+        int res = 0;
+        for (int i = 0; i < points.length; i++) {
+            map.clear();
+            int overlap = 0;
+            int max = 0;
+            for (int j = i + 1; j < points.length; j++) {
+                int x = points[j].x - points[i].x;
+                int y = points[j].y - points[i].y;
+                if (x == 0 && y == 0) {
+                    overlap++;
+                    continue;
+                }
+                int gcd = gcd(x, y);
+                if (gcd != 0) {
+                    x /= gcd;
+                    y /= gcd;
+                }
+                if (map.containsKey(x)) {
+                    if (map.get(x).containsKey(y)) {
+                        map.get(x).put(y, map.get(x).get(y) + 1);
+                    } else {
+                        map.get(x).put(y, 1);
+                    }
+                } else {
+                    Map<Integer, Integer> mymap = new HashMap<>();
+                    mymap.put(y, 1);
+                    map.put(x, mymap);
+                }
+                max = Math.max(max, map.get(x).get(y));
+            }
+            res = Math.max(res, max + overlap + 1);
+        }
+        return res;
+    }
+    public int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+}

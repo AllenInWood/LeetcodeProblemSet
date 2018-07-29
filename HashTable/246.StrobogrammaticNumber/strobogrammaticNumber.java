faac0f78b3d42c4d657052bb48e43e1b24334d06
alleninwood <alleninwood@gmail.com>
2018-07-29 01:32:02 -0700

    07/28/18

---
+++ HashTable/246.StrobogrammaticNumber/strobogrammaticNumber.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isStrobogrammatic(String num) {
+        Map<Character, Character> map = new HashMap<>();
+        map.put('6', '9');
+        map.put('9', '6');
+        map.put('0', '0');
+        map.put('1', '1');
+        map.put('8', '8');
+        
+        int low = 0, high = num.length() - 1;
+        while (low <= high) {
+            if (!map.containsKey(num.charAt(low))) {
+                return false;
+            }
+            if (map.get(num.charAt(low))
+                != num.charAt(high)) {
+                return false;
+            }
+            low++;
+            high--;
+        }
+        return true;
+    }
+}
